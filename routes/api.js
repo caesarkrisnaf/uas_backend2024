90491d6a57a01182684903bac7c31fb66d472cb3
caesarkrisnaf <caesarkrisna08@gmail.com>
2024-01-19 12:46:30 +0700

    UAS-PemrogramanBackend-2024

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 // import EmployeeController
-
+const EmployeeController = require("../controllers/EmployeeController");
 // import express
 const express = require("express");
+const Employee = require("../models/Employee");
 
 // membuat object router
 const router = express.Router();
@@ -14,6 +15,14 @@ router.get("/", (req, res) => {
 });
 
 // Membuat routing employee
-
+router.get("/employees", EmployeeController.index);
+router.post("/employees", EmployeeController.store);
+router.delete("/employees/:id", EmployeeController.destroy);
+router.get("/employees/:id", EmployeeController.show);
+router.put("/employees/:id", EmployeeController.update);
+router.get("/employees/status/active", EmployeeController.active);
+router.get("/employees/status/inactive", EmployeeController.inactive);
+router.get("/employees/status/terminated",EmployeeController.terminated);
+router.get("/employees/search/:name",EmployeeController.search);
 // export router
 module.exports = router;
