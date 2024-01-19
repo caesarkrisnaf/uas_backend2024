53a1220a9073de21da9b92b0da8e96d24d097f01
caesarkrisnaf <caesarkrisna08@gmail.com>
2024-01-19 12:52:55 +0700

    UAS_PEMROGRAMAN-BACKEND-2024

--- models/Employee.js
+++ models/Employee.js
@@ -5,85 +5,84 @@ const db = require("../config/database");
 class Employee {
   static all() {
     return new Promise((resolve, reject) => {
-        // lakukan query ke db untuk ambil data
-        const sql = "SELECT * FROM employees";
-        db.query(sql, (sql, results) => {
-            resolve(results);
-        });
+      // lakukan query ke db untuk ambil data
+      const sql = "SELECT * FROM employees";
+      db.query(sql, (sql, results) => {
+        resolve(results);
+      });
     });
-}
+  }
 
-static async create(Employee) {
-  const id = await new Promise((resolve, reject) => {
+  static async create(Employee) {
+    const id = await new Promise((resolve, reject) => {
       const sql = "INSERT INTO employees SET ?";
       db.query(sql, Employee, (err, results) => {
-          resolve(results.insertid);
+        resolve(results.insertid);
       });
-  });
-
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT * FROM employees WHERE id = ?`;
-    db.query(sql, id, (err, results) => {
-        resolve(results);
     });
-});
-}
 
-static find(id) {
-  // lakukan promise, select by id
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const sql = `SELECT * FROM employees WHERE id = ?`;
       db.query(sql, id, (err, results) => {
-          resolve(results[0]);
+        resolve(results);
       });
-  });
-}
+    });
+  }
 
-static async delete(id) {
-  // query delete
-  return new Promise((resolve, reject) => {
+  static find(id) {
+    // lakukan promise, select by id
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT * FROM employees WHERE id = ?`;
+      db.query(sql, id, (err, results) => {
+        resolve(results[0]);
+      });
+    });
+  }
+
+  static async delete(id) {
+    // query delete
+    return new Promise((resolve, reject) => {
       // query sql
       const sql = "DELETE FROM employees WHERE id = ?";
       db.query(sql, id, (err, results) => {
-          resolve(results);
+        resolve(results);
       });
-  });
-}
+    });
+  }
 
-static async update(id, data) {
-  // update data
-  await new Promise((resolve, reject) => {
+  static async update(id, data) {
+    // update data
+    await new Promise((resolve, reject) => {
       // query untuk update data
       const sql = "UPDATE employees SET ? WHERE id = ?";
       db.query(sql, [data, id], (err, results) => {
-          resolve(results);
+        resolve(results);
       });
-  });
+    });
 
-  // select data by id
-  const employee = await this.find(id);
-  return employee;
-}
+    // select data by id
+    const employee = await this.find(id);
+    return employee;
+  }
 
-static status_employe(status_employe) {
-  return new Promise((resolve, reject) => {
-    // Lakukan query ke db untuk ambil data
-    const sql = "SELECT * FROM employees WHERE status = ?";
-    db.query(sql, status_employe, (sql, result) => {
-      resolve(result);
-    });
-    });
-  }
+  static status_employe(status_employe) {
+    return new Promise((resolve, reject) => {
+      // Lakukan query ke db untuk ambil data
+      const sql = "SELECT * FROM employees WHERE status = ?";
+      db.query(sql, status_employe, (sql, result) => {
+        resolve(result);
+      });
+    });
+  }
 
-static search(name) {
-  // lakukan promise, select by id
-  return new Promise((resolve, reject) => {
+  static search(name) {
+    return new Promise((resolve, reject) => {
       const sql = `SELECT * FROM employees WHERE name = ?`;
       db.query(sql, name, (err, results) => {
-          resolve(results[0]);
+        resolve(results[0]);
       });
-  });
-}
+    });
+  }
 
 }
 
